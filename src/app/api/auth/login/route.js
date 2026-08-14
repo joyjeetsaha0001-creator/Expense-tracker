c98b464172a1b1119e8990423398bb219ca09d04
joyjeetsaha0001 <joyjeetsaha0001@gmail.com>
2026-08-14 18:59:21 +0530

    api/auth/login/route.js and api/auth/me/route.js modified

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -4,58 +4,84 @@ import jwt from "jsonwebtoken";
 import { ZodError } from "zod";
 import { connectDB } from "@/lib/mongodb";
 import User from "@/models/User";
-import {loginSchema} from "@lib/validations";
+import { loginSchema } from "@lib/validations";
 import { generateToken } from "@/lib/auth";
 
 
-export async function POST(request){
+export async function POST(request) {
+    try {
 
-    const body=await request.json();
+        const body = await request.json();
 
-    const validData = loginSchema.parse(body);
+        const validData = loginSchema.parse(body);
 
-    const {email,password}=validData;
+        const { email, password } = validData;
 
-    await connectDB();
+        await connectDB();
 
-    const user=await User.findOne({email});
+        const user = await User.findOne({ email });
 
-    if(!user){
-        return NextResponse.json(
-            {
-                message:"Invalid email or password"
-            },
-            {
-                status:401
-            }
-        );
+        if (!user) {
+            return NextResponse.json(
+                {
+                    message: "Invalid email or password"
+                },
+                {
+                    status: 401
+                }
+            );
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return NextResponse.json(
+                {
+                    message: "Invalid email or password"
+                },
+                {
+                    status: 401
+                }
+            )
+        }
+
+        const token = generateToken(user._id);
+
+
+        const response = NextResponse.json({
+            message: "Login successful"
+        });
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 60 * 60 * 24 * 7,
+            path: "/",
+        });
     }
+    catch (error) {
 
-    const isMatch= await bcrypt.compare(password,user.password);
+        if (error instanceof ZodError) {
+            return NextResponse.json(
+                {
+                    errors: error.flatten().fieldErrors,
+                },
+                {
+                    status: 400
+                }
+            )
+        }
 
-    if(!isMatch){
+        console.log(error);
+
         return NextResponse.json(
             {
-                message:"Invalid email or password"
+                message: "Internal server error"
             },
             {
-                status:401
+                status: 500
             }
         )
     }
-
-    const token = generateToken(user._id);
-
-
-    const response = NextResponse.json({
-        message:"Login successful"
-    });
-
-    response.cookies.set("token",token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==="production",
-        sameSite:"strict",
-        maxAge:60 * 60 * 24 * 7,
-        path:"/",
-    });
 }
--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -4,36 +4,53 @@ import { verifyToken } from "@/lib/auth";
 import { connectDB } from "@/lib/mongodb";
 import User from "@/models/User";
 
-const cookieStore = await cookies();
 
-const token = cookieStore.get("token")?.value;
+export async function GET(){
+    try{
+        const cookieStore = await cookies();
 
-if(!token){
-    return NextResponse.json(
-        {
-            message:"Unauthorized"
-        },
-        {
-            status:401
+        const token = cookieStore.get("token")?.value;
+
+        if(!token){
+            return NextResponse.json(
+                {
+                    message:"Unauthorized"
+                },
+                {
+                    status:401
+                }
+            );
         }
-    );
-}
 
-const decoded = verifyToken(token);
+        const decoded = verifyToken(token);
 
-if(!decoded){
-    return NextResponse.json(
-        {
-            message:"Invalid Token"
-        },
-        {
-            status:401
+        if(!decoded){
+            return NextResponse.json(
+                {
+                    message:"Invalid Token"
+                },
+                {
+                    status:401
+                }
+            )
         }
-    )
+
+        await connectDB();
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        return NextResponse.json(user);
+    }
+    catch(error){
+        console.log("Auth Me Error:",error);
+
+        return NextResponse.json(
+            {
+                message:"Internal Server Error"
+            },
+            {
+                status:500
+            }
+        )
+    }
 }
-
-await connectDB();
-
-const user = await User.findById(decoded.userId).select("-password");
-
-return NextResponse.json(user);
