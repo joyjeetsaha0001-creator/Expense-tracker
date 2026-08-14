82367a19d255e469dc3da009024301065088b73d
joyjeetsaha0001 <joyjeetsaha0001@gmail.com>
2026-08-14 19:10:00 +0530

    getCurrentUser() method added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,9 @@
 import jwt from "jsonwebtoken";
+import {cookies} from "next/headers";
+import User from "@/models/User";
+import {connectDB} from "@/lib/mongodb";
+
+
 
 export function generateToken(userId){
     const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"});
@@ -12,5 +17,33 @@ export function verifyToken(token){
     }
     catch{
         return null;
+    }
+}
+
+export async function getCurrentUser(){
+    try{
+        const cookieStore=await cookies();
+        const token=cookiStore.get("token")?.value;
+
+        if(!token){
+            return null;
+        }
+
+        const decoded=verifyToken(token);
+        
+        if(!decoded){
+            return null;
+        }
+
+        await connectDB();
+
+        const user=await User.findById(decoded.userId).select("-password");
+        if(!user){
+            return null;
+        }
+        return user;
+    }
+    catch(error){
+        console.log("Get Current User Error:",error);
     }
 }
