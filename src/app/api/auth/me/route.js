5c949ce1a6300eda8a0fe1feef6cf39b2bdb76c4
joyjeetsaha0001 <joyjeetsaha0001@gmail.com>
2026-08-14 19:17:57 +0530

    api/auth/me modified with getCurrentUser() method

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -3,15 +3,14 @@ import { NextResponse } from "next/server";
 import { verifyToken } from "@/lib/auth";
 import { connectDB } from "@/lib/mongodb";
 import User from "@/models/User";
+import { getCurrentUser } from "@/lib/auth";
 
 
 export async function GET(){
     try{
-        const cookieStore = await cookies();
+        const user=getCurrentUser();
 
-        const token = cookieStore.get("token")?.value;
-
-        if(!token){
+        if(!user){
             return NextResponse.json(
                 {
                     message:"Unauthorized"
@@ -21,25 +20,8 @@ export async function GET(){
                 }
             );
         }
+       return NextResponse.json(user);
 
-        const decoded = verifyToken(token);
-
-        if(!decoded){
-            return NextResponse.json(
-                {
-                    message:"Invalid Token"
-                },
-                {
-                    status:401
-                }
-            )
-        }
-
-        await connectDB();
-
-        const user = await User.findById(decoded.userId).select("-password");
-
-        return NextResponse.json(user);
     }
     catch(error){
         console.log("Auth Me Error:",error);
