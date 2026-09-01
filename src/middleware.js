83d276f13e0477ef0b251bc0e547023471f9ff15
joyjeetsaha0001 <joyjeetsaha0001@gmail.com>
2026-09-01 12:15:26 +0530

    Middleware fixed

--- src/middleware.js
+++ src/middleware.js
@@ -1,32 +1,23 @@
-import {NextResponse} from "next/server";
-import {verifyToken} from "@/lib/auth";
+import { NextResponse } from "next/server";
 
-export function middleware(request){
-    try{
-        const token = request.cookies.get("token")?.value;
-        if(!token){
-            return NextResponse.json(
-                new URL("/login",request.url)
-            );
-        }
+export function middleware(request) {
+  const token = request.cookies.get("token")?.value;
 
-        const decoded=verifyToken(token);
+  if (!token) {
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+  }
 
-        if(!decoded){
-            return NextResponse.json(
-                new URL("/login",request.url)
-            );
-        }
-        return NextResponse.next();
-    }
+  return NextResponse.next();
 }
 
-export const config={
-    matcher:[
-        "/dashboard/:path*",
-        "/transactions/:path*",
-        "/categories/:path*",
-        "/reports/:path*",
-        "/settings/:path*",
-    ],
+export const config = {
+  matcher: [
+    "/dashboard/:path*",
+    "/transactions/:path*",
+    "/categories/:path*",
+    "/reports/:path*",
+    "/settings/:path*",
+  ],
 };
